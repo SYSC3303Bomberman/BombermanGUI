ffd0faeb4a9cc266ad392dabd8189f34facaa080
danielabdalla <danielabdalla@cb5109-pc23.labs.sce.carleton.ca>
2014-03-06 14:40:13 -0500

    Bomberman GUI

---
+++ BombermanGUI/src/BombGUI.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class BombGUI extends JFrame {
+	JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JLabel labels[] = new JLabel[30];
+	
+	public BombGUI(){
+		frame.setSize(400, 400);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		panel.setLayout(new GridLayout(5,6));
+		panel.setBorder(BorderFactory.createEmptyBorder(2,2,2,2));
+		for(int i = 0; i < 30; i++){
+			labels[i] = new JLabel();
+			labels[i].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+			panel.add(labels[i]);
+		}
+		frame.add(panel);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new BombGUI();
+
+	}
+
+}
